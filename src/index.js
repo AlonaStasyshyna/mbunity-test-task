f82ba9b44ffc49c31f45e4a38a3db554d5f5e49a
AlonaStasyshyna <alena.stasishyna28@gmail.com>
2023-04-02 19:28:54 +0300

    Fix issues

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { GlobalStyle } from 'utils/GlobalStyle.styled';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/mbunity-test-task">
+    <BrowserRouter basename="/mbunity-test-task/contacts">
       <GlobalStyle />
       <App />
     </BrowserRouter>
